441879e8434004ca69dc3bfffc095ea58ced2c6e
seizans <seizans@gmail.com>
2012-11-25 18:52:08 +0900

    Create AllNumPerAge data type.

--- Model.hs
+++ Model.hs
@@ -4,6 +4,7 @@
 
 module Model where
 
+import Data.Monoid (Monoid(..))
 import Data.Text (Text)
 import Database.Persist.Sqlite
 import Database.Persist.TH
@@ -27,6 +28,28 @@ data Sex = Male | Female
 
 data AgeArea = Over0 | Over20 | Over40 | Over60
   deriving (Show, Read, Eq, Ord, Enum, Bounded)
+
+data AllNumPerAge = AllNumPerAge
+    { over0Male :: Int
+    , over0Female :: Int
+    , over20Male :: Int
+    , over20Female :: Int
+    , over40Male :: Int
+    , over40Female :: Int
+    , over60Male :: Int
+    , over60Female :: Int
+    }
+instance Monoid AllNumPerAge where
+    mempty = AllNumPerAge 0 0 0 0 0 0 0 0
+    mappend x y = AllNumPerAge
+        (over0Male x    + over0Male y)
+        (over0Female x  + over0Female y)
+        (over20Male x   + over20Male y)
+        (over20Female x + over20Female y)
+        (over40Male x   + over40Male y)
+        (over40Female x + over40Female y)
+        (over60Male x   + over60Male y)
+        (over60Female x + over60Female y)
 
 derivePersistField "Sex"
 derivePersistField "AgeArea"
